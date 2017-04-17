c259a651d32fe29e070796ab0b4aefb7c5c7f857
Nimesh Kiran Verma <nimesh.aug11@gmail.com>
2017-04-17 14:44:15 +0530

    Loan Specification Done

--- upwards/loan_product/v1/views.py
+++ upwards/loan_product/v1/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 
 from . import serializers
 from loan_product import models
+from services import loan_specifications_service
 from common.v1.decorators import session_authorize, meta_data_response, catch_exception
 from activity.models import register_customer_state
 from activity.model_constants import ELIGIBILITY_REJECTED_LOAN_PRODUCT_SUBMIT_STATE, LOAN_SPECIFICATION_REVIEW_STATE
@@ -76,12 +77,15 @@ class LoanSpecifications(APIView):
     @session_authorize()
     def post(self, request, auth_data):
         if auth_data.get('authorized'):
-            dummy = {
-                "principal_amount": 10000,
-                "tenure": 20,
-                "emi": 550,
-            }
             register_customer_state(
                 LOAN_SPECIFICATION_REVIEW_STATE, auth_data['customer_id'])
-            return Response(dummy, status=status.HTTP_200_OK)
+            return Response({}, status=status.HTTP_200_OK)
+        return Response({}, status.HTTP_401_UNAUTHORIZED)
+
+    # @catch_exception(LOGGER)
+    @meta_data_response()
+    @session_authorize()
+    def get(self, request, auth_data, *args, **kwargs):
+        if auth_data.get('authorized'):
+            return Response(loan_specifications_service.LoanSpecifications(auth_data['customer_id']).data, status=status.HTTP_200_OK)
         return Response({}, status.HTTP_401_UNAUTHORIZED)
